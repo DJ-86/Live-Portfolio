33ae9f61bec129054f71617bce90711604ca9fc0
DJPC <davidmarkjones86@gmail.com>
2023-05-30 11:25:40 +0100

    Added logo and dropdown

--- site/src/components/Nav.jsx
+++ site/src/components/Nav.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import logo from '../assets/logo.svg';
+import DropdownNavigation from "./DropdownNavigation";
+
 
 function Nav() {
     return (
         <>
+        <img src={logo} alt="logo" />
+        <DropdownNavigation></DropdownNavigation>
         <h2>Nav working</h2>
         </>
     )
